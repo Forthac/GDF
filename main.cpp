fa2d83ed10a90ba92d8b18932ac05bfa7051d77c
Forthac <Forthac@users.noreply.github.com>
2023-04-24 18:22:11 -0700

    Added fragment shader
    Added helper function in main.cpp to load shader
    
    Added call in main to load shader.
    
    Added bitmap for initial screen.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+std::string loadShaderSource(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        throw std::runtime_error("Failed to open shader file: " + filename);
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 int main() {
     if (!glfwInit()) {
@@ -11,8 +25,9 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    std::string gameOfLifeFragmentShaderSource = loadShaderSource("game_of_life.frag");
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Your Project Name", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GDF", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
