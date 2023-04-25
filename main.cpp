10aec611811bfe5ff055b8ab339644f173505d5a
Forthac <Forthac@users.noreply.github.com>
2023-04-24 21:14:45 -0700

    Refactored while loop

--- main.cpp
+++ main.cpp
@@ -5,6 +5,46 @@
 #include "texture.h"
 #include "quad.h"
 
+void updateUniforms(GLuint shaderProgram, int width, int height) {
+    GLint textureSizeLocation = glGetUniformLocation(shaderProgram, "textureSize");
+    glUniform2f(textureSizeLocation, (float)width, (float)height);
+}
+
+void renderToTexture(GLuint framebuffer, GLuint shaderProgram, GLuint texture, GLuint VAO, int width, int height) {
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glViewport(0, 0, width, height);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    updateUniforms(shaderProgram, width, height);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
+
+void renderToScreen(GLuint shaderProgram, GLuint texture, GLuint VAO, int width, int height) {
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glViewport(0, 0, width, height);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    updateUniforms(shaderProgram, width, height);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+}
 
 int main() {
     if (!glfwInit()) {
@@ -49,43 +89,13 @@ int main() {
 
     while (!glfwWindowShouldClose(window)) {
         // Render to the next state texture
-        glBindFramebuffer(GL_FRAMEBUFFER, framebuffers[1 - currentTexture]);
-        glViewport(0, 0, width, height);
-
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-        GLint textureSizeLocation = glGetUniformLocation(shaderProgram, "textureSize");
-        glUniform2f(textureSizeLocation, (float)width, (float)height);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, textures[currentTexture]);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        renderToTexture(framebuffers[1 - currentTexture], shaderProgram, textures[currentTexture], VAO, width, height);
 
         // Swap current and next state textures
         currentTexture = 1 - currentTexture;
 
         // Render the current state texture to the default framebuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glViewport(0, 0, 800, 600);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-
-        glUniform2f(textureSizeLocation, (float)width, (float)height);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, textures[currentTexture]);
-
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        renderToScreen(shaderProgram, textures[currentTexture], VAO, 800, 600);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
